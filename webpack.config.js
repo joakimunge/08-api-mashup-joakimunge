4942115573f75af262e2a906758771214ef65cc2
joakimunge <ashba@unge.se>
2018-01-29 18:08:54 +0100

    Init structure

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const webpack = require('webpack');
+const path = require('path');
+const glob = require('glob');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const PurifyCSSPlugin = require('purifycss-webpack');
+const inProduction = (process.env.NODE_ENV === 'production');
+
+
+module.exports = {
+    entry:  {
+        app: [
+            './src/app.js', 
+            './src/styles/main.scss'
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/,
+                use: ExtractTextPlugin.extract({
+                    use: ['css-loader', 'sass-loader'],
+                    fallback:'style-loader'
+                })
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'file-loader',
+                options: {
+                    name: './resources/[name].[ext]',
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('[name].css'),
+        new webpack.LoaderOptionsPlugin({
+            minimize: inProduction
+        }),
+        new PurifyCSSPlugin({
+            // Give paths to parse for rules. These should be absolute!
+            paths: glob.sync(path.join(__dirname, 'index.html')), // Change this for bigger projects to a folder e.g. app/*.html
+            minimize: inProduction
+        }),
+        new CleanWebpackPlugin(['dist'], {
+            root: __dirname,
+            verbose: true,
+            dry: false
+        })
+    ]
+};
+
+if (inProduction) {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin()
+    );
+}
