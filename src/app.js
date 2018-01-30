1c2234923494ec65dcd39afb71c6155fab5b989d
Joakim Unge <ashba@unge.se>
2018-01-30 14:36:50 +0100

    Functionality to set background based on query

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ class App {
   constructor() {
     this.form = document.querySelector('form');
     this.input = document.querySelector('.search__input');
+    this.background  = document.querySelector('.hero__background');
     this.flickr = new Flickr('9937eb786c73b10e099965d8e6e48a40', this);
     this.thesaurus = new Thesaurus('78f3d8b49dc89abf9207cb09291eea61', this);
     this.initialize();
@@ -29,8 +30,9 @@ class Flickr {
 
   getPhotosFromQuery() {
     const query = this.caller.input.value;
+    const sort = '&sort=interestingness-desc'
     const url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=';
-    const params = '&text=' + query + '&extras=url_q&format=json&nojsoncallback=1';
+    const params = '&text=' + query + sort + '&extras=url_q&format=json&nojsoncallback=1';
     const flickrUrl = url + this.apikey + params;
 
     fetch(flickrUrl)
@@ -38,12 +40,26 @@ class Flickr {
       .then(res => {
         console.log('success!');
         console.log(res);
+        this.setBackground(res);
       })
       .catch(err => console.log(err));
   }
 
-  setBackground() {
-    
+  setBackground(res) {
+    const farmId = res.photos.photo[5].farm;
+    const serverId = res.photos.photo[5].server;
+    const id = res.photos.photo[5].id;
+    const secret = res.photos.photo[5].secret;
+    const url = `https://farm${farmId}.staticflickr.com/${serverId}/${id}_${secret}.jpg`;
+    console.log(this.caller);
+    this.caller.background.style = `
+      background: url(${url}) no-repeat;
+      background-size: cover;
+      -webkit-background-size: cover;
+      -moz-background-size: cover; 
+      -o-background-size: cover;
+    `;
+
   }
 }
 
