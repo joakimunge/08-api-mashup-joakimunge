5be02a0abdd1bbe8482b6667ff6acf1d826f6041
Joakim Unge <ashba@unge.se>
2018-01-30 14:12:10 +0100

    Add basic js structure

---
+++ src/app.js
@@ -0,0 +1,76 @@
+class App {
+  constructor() {
+    this.form = document.querySelector('form');
+    this.input = document.querySelector('.search__input');
+    this.flickr = new Flickr('9937eb786c73b10e099965d8e6e48a40', this);
+    this.thesaurus = new Thesaurus('78f3d8b49dc89abf9207cb09291eea61', this);
+    this.initialize();
+  }
+
+  initialize() {
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    this.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      this.flickr.getPhotosFromQuery();
+      this.thesaurus.getWordsFromQuery();
+    });
+  }
+
+}
+
+class Flickr {
+  constructor(apiKey, caller) {
+    this.apikey = apiKey;
+    this.caller = caller;
+  }
+
+  getPhotosFromQuery() {
+    const query = this.caller.input.value;
+    const url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=';
+    const params = '&text=' + query + '&extras=url_q&format=json&nojsoncallback=1';
+    const flickrUrl = url + this.apikey + params;
+
+    fetch(flickrUrl)
+      .then(res => res.json())
+      .then(res => {
+        console.log('success!');
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  }
+
+  setBackground() {
+    
+  }
+}
+
+class Thesaurus {
+  constructor(apiKey, caller) {
+    this.apiKey = apiKey;
+    this.caller = caller;
+    this.related = document.querySelector('.search__related__list');
+  }
+
+  getWordsFromQuery() {
+    const query = this.caller.input.value;
+    const url = 'http://words.bighugelabs.com/api/2/78f3d8b49dc89abf9207cb09291eea61/';
+    const thesaurusUrl = url + query + '/json';
+
+    fetch(thesaurusUrl)
+      .then(res => res.json())
+      .then(res => {
+        console.log('success!');
+        this.render(res);
+      })
+      .catch(err => console.log(err));
+  }
+
+  render(res) {
+    console.log(res);
+  }
+}
+
+new App();
