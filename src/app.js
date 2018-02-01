a6bc687f85e62116812efbc8c512674d4d8a906f
Joakim Unge <ashba@unge.se>
2018-02-01 23:15:21 +0100

    Refactor search method

--- src/app.js
+++ src/app.js
@@ -5,11 +5,14 @@ import Photo from './components/Photo';
 
 class App {
   constructor() {
-    this.form = document.querySelector('#form__search__main');
-    this.search = document.querySelector('.search');
-    this.input = document.querySelector('.search__input');
+    this.formMain = document.querySelector('#form__search__main');
+    this.formNav = document.querySelector('#form__search__nav');    
+    this.searchSection = document.querySelector('.search');
+    this.inputMain = document.querySelector('.search__input');
+    this.inputNav = document.querySelector('#input__nav');    
     this.navbar = document.querySelector('.navbar');
     this.background = document.querySelector('.hero__background');
+    this.related = document.querySelector('.search__related__list');
     this.flickr = new Flickr(process.env.FLICKR_API, this);
     this.thesaurus = new Thesaurus(process.env.THSRS_API, this);
     this.unsplash = new Unsplash(process.env.UNSPLASH_API, this);
@@ -21,19 +24,36 @@ class App {
   }
 
   addEventListeners() {
-    this.form.addEventListener('submit', (e) => {
+    this.formMain.addEventListener('submit', (e) => {
+      this.changeLayout(e);
+      this.search(this.inputMain.value);
+    });
+    this.formNav.addEventListener('submit', (e) => {
       e.preventDefault();
-      this.navbar.style.display = 'block';
-      this.background.style = '';
-      this.search.className += ' fadeOut';
-      this.flickr.getPhotosFromQuery(this.input.value);
-      this.thesaurus.getWordsFromQuery(this.input.value);
+      this.search(this.inputNav.value);
     });
-    this.search.addEventListener('animationend', (e) => {
-      this.search.style.display = 'none';
+    this.searchSection.addEventListener('animationend', (e) => {
+      if(e.animationName == 'fadeOut') {
+        this.searchSection.style.display = 'none';
+      }
     });
-
+    this.related.addEventListener('click', (e) => {
+      this.search(e.target.textContent);
+    })
   }
+
+  changeLayout(e) {
+    e.preventDefault();
+    this.navbar.style.display = 'block';
+    this.background.style = '';
+    this.searchSection.className += ' fadeOut';
+  }
+
+  search(query) {
+    this.flickr.getPhotosFromQuery(query);
+    this.thesaurus.getWordsFromQuery(query);
+  }
+  
 
   // Add api calls to array
   // Return fetch from api calls
