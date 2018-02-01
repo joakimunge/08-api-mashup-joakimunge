c6d976d063d48a597e3b7219e016ed9def7d2fae
Joakim Unge <ashba@unge.se>
2018-02-01 18:12:31 +0100

    Add Unsplash component

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 import Flickr from './components/Flickr';
 import Thesaurus from './components/Thesaurus';
+import Unsplash from './components/Unsplash';
 import Photo from './components/Photo';
 
 class App {
   constructor() {
     this.form = document.querySelector('form');
+    this.search = document.querySelector('.search');
     this.input = document.querySelector('.search__input');
     this.background = document.querySelector('.hero__background');
     this.flickr = new Flickr(process.env.FLICKR_API, this);
     this.thesaurus = new Thesaurus(process.env.THSRS_API, this);
+    this.unsplash = new Unsplash(process.env.UNSPLASH_API, this);
     this.initialize();
   }
 
@@ -19,6 +22,8 @@ class App {
   addEventListeners() {
     this.form.addEventListener('submit', (e) => {
       e.preventDefault();
+      this.background.style = '';
+      this.search.className += ' slideCorner';
       this.flickr.getPhotosFromQuery(this.input.value);
       this.thesaurus.getWordsFromQuery(this.input.value);
     });
